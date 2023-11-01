0a1495ec46c59bb999702f73702cb961496b22ae
Gabriel Dugny <git@dugny.me>
2023-11-01 16:19:50 +0100

    chore: move setuptools config in pyproject.toml, target Linux and Windows ARM

--- setup.py
+++ setup.py
@@ -2,38 +2,12 @@
 Py-Tree-sitter
 """
 
-from os import path
 from platform import system
 
 from setuptools import Extension, setup
 
-with open(path.join(path.dirname(__file__), "README.md")) as f:
-    LONG_DESCRIPTION = f.read()
-
+# Only ext_modules is not supported in pyproject.toml
 setup(
-    name="tree_sitter",
-    version="0.20.2",
-    maintainer="Max Brunsfeld",
-    maintainer_email="maxbrunsfeld@gmail.com",
-    author="Max Brunsfeld",
-    author_email="maxbrunsfeld@gmail.com",
-    url="https://github.com/tree-sitter/py-tree-sitter",
-    license="MIT",
-    platforms=["any"],
-    python_requires=">=3.3",
-    description="Python bindings for the Tree-Sitter parsing library",
-    long_description=LONG_DESCRIPTION,
-    long_description_content_type="text/markdown",
-    classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Topic :: Software Development :: Compilers",
-        "Topic :: Text Processing :: Linguistic",
-    ],
-    install_requires=[
-        "setuptools>=60.0.0; python_version>='3.12'"
-    ],
-    packages=["tree_sitter"],
-    package_data={"tree_sitter": ["py.typed", "*.pyi"]},
     ext_modules=[
         Extension(
             "tree_sitter.binding",
@@ -44,5 +18,4 @@ setup(
             ),
         )
     ],
-    project_urls={"Source": "https://github.com/tree-sitter/py-tree-sitter"},
 )
